a1ac32ba371428275a54c81c94832e69e7e7a413
maskikuy <kproject305@gmail.com>
2026-01-31 09:33:26 +0700

    menambahkan entitas di kategori

--- app/src/main/java/com/example/sistemmanajemen/Data/Entity/Category.kt
+++ app/src/main/java/com/example/sistemmanajemen/Data/Entity/Category.kt
@@ -1,2 +1,12 @@
 package com.example.sistemmanajemen.Data.Entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "categories")
+data class Category(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val parentId: Int? = null,
+    val isDeleted: Boolean = false
+)
