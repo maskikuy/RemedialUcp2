0398463285196d77f064e5500459b4b11f1e5262
maskikuy <kproject305@gmail.com>
2026-01-31 09:36:40 +0700

    menambahkan entitas

--- app/src/main/java/com/example/sistemmanajemen/Data/Entity/BookInstance.kt
+++ app/src/main/java/com/example/sistemmanajemen/Data/Entity/BookInstance.kt
@@ -1,2 +1,13 @@
 package com.example.sistemmanajemen.Data.Entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "book_instances")
+data class BookInstance(
+    @PrimaryKey(autoGenerate = true) val instanceId: Int = 0,
+    val bookId: Int,
+    val uniqueCode: String, // ID Unik
+    val status: String, // "Available" atau "Borrowed"
+    val isDeleted: Boolean = false
+)
