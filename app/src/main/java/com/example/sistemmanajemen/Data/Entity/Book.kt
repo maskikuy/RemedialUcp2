3b395a344a20440ef0d4da436b3545a285fb5bec
maskikuy <kproject305@gmail.com>
2026-01-31 09:36:09 +0700

    menambahkan entitas di book

--- app/src/main/java/com/example/sistemmanajemen/Data/Entity/Book.kt
+++ app/src/main/java/com/example/sistemmanajemen/Data/Entity/Book.kt
@@ -1,2 +1,12 @@
 package com.example.sistemmanajemen.Data.Entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "books")
+data class Book(
+    @PrimaryKey(autoGenerate = true) val bookId: Int = 0,
+    val title: String,
+    val categoryId: Int?, // Nullable untuk opsi "Tanpa Kategori"
+    val isDeleted: Boolean = false
+)
